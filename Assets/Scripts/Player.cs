af4c956ebfa12cddbde8cf0cb57eb1437caeec47
s1071657 <72445023+s1071657@users.noreply.github.com>
2020-12-07 19:34:59 +0800

    1207_FinalChooes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
     public GameObject myNote;
     bool isOpen;
     public Camera cam;
+    bool canMove = true;
     [SerializeField] private float timeHit = 0f;
 
     void Start()
@@ -26,28 +27,29 @@ public class Player : MonoBehaviour
         PlayerMove();                     
     }
     void PlayerMove() {
-        float h = Input.GetAxis("Horizontal");   //取得輸入器的水平軸值(角色轉左/轉右)
-        float v = Input.GetAxis("Vertical");     //取得輸入器的垂直軸值(角色前進/後退)
-        float x = Input.GetAxis("Mouse X");
-        float y = Input.GetAxis("Mouse Y");
-        //print(h + "/"+ v);
-        
-        transform.Translate(v * Vector3.forward * MoveSpeed * Time.deltaTime); //角色移動(跑步)
-        transform.Translate(h * Vector3.right * MoveSpeed * Time.deltaTime);
-        
+        if (canMove) {
+            float h = Input.GetAxis("Horizontal");   //取得輸入器的水平軸值(角色轉左/轉右)
+            float v = Input.GetAxis("Vertical");     //取得輸入器的垂直軸值(角色前進/後退)
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+            //print(h + "/"+ v);
 
-        transform.Rotate(0, x * RotateSpeed, 0); //角色旋轉
+            transform.Translate(v * Vector3.forward * MoveSpeed * Time.deltaTime); //角色移動(跑步)
+            transform.Translate(h * Vector3.right * MoveSpeed * Time.deltaTime);
 
 
-        if (Input.GetButtonDown("Jump")) //角色跳躍
-        {
-            if (JumpAction == false)
-            {//不在跳躍狀態
-                rb.AddForce(Vector3.up * JumpPower); //增加角色向上跳的力量
-                JumpAction = true;//設定跳躍狀態(跳躍中)
+            transform.Rotate(0, x * RotateSpeed, 0); //角色旋轉
+
+
+            if (Input.GetButtonDown("Jump")) //角色跳躍
+            {
+                if (JumpAction == false)
+                {//不在跳躍狀態
+                    rb.AddForce(Vector3.up * JumpPower); //增加角色向上跳的力量
+                    JumpAction = true;//設定跳躍狀態(跳躍中)
+                }
             }
         }
-
     }
 
     void OpenMyNote()
@@ -57,5 +59,13 @@ public class Player : MonoBehaviour
             isOpen = !isOpen;
             myNote.SetActive(isOpen);
         }
+    }
+
+    void CanNotMove() {
+        canMove = false;
+    }
+    void CanMove()
+    {
+        canMove = true;
     }
 }
